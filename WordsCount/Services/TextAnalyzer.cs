c69060707fba9697ef780484eb864b92bb4d28e8
Sergiy Sukharskyi <sergiygame2@gmail.com>
2017-09-28 22:30:18 +0300

    LAB1: added text analyzer

---
+++ WordsCount/Services/TextAnalyzer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace WordsCount.Services
+{
+    public class TextAnalyzer
+    {
+        private readonly string _text;
+        private static readonly char[] SymbolsDelimeters;
+        private static readonly char[] WordsDelimeters;
+        private static readonly char[] LinesDelimeters;
+        
+        static TextAnalyzer()
+        {
+            LinesDelimeters = new[] {'\n'};
+            SymbolsDelimeters = new[] {' ', '\r', '\n'};
+            WordsDelimeters = new[] { ' ', '.', ',', '?', '!', '\r', '\n' };
+        }
+
+        public TextAnalyzer(string text) => _text = text.Trim();
+
+        public int CountSymbols() => _text.Count(c => !SymbolsDelimeters.Contains(c));
+
+        public int CountWords() => _text.Split(WordsDelimeters, StringSplitOptions.RemoveEmptyEntries).Length;
+
+        public int CountLines() => _text.Split(LinesDelimeters).Length;
+    }
+}
